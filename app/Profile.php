a543c4fcc4c29e21773f46da15dd8616ac2af7c2
ctkrecords <ctkrecordings@gmail.com>
2020-01-31 16:25:17 -0600

    Added profileImage function

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+
+    public function profileImage(){
+        $imagePath = ($this->image) ? $this->image : 'profile/lhq51k0vz1q3KC5DxHS68xlYpi9FrVY0MASFFfnl.jpeg';
+        return '/storage/' . $imagePath;
+        
+    }
+
     protected $guarded = [];
     public function user(){
         return $this->belongsTo(User::class);
