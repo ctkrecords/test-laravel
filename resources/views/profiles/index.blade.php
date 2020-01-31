cc98a6fb8d2728e1d7c887a85eb7d703ce979646
ctkrecords <ctkrecordings@gmail.com>
2020-01-31 14:51:10 -0600

    Added the policy for the edit and update actions

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,14 +4,18 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5 text-center">
-            <img src="https://scontent-mia3-2.cdninstagram.com/v/t51.2885-19/s150x150/70985486_577637296311063_2240788552625422336_n.jpg?_nc_ht=scontent-mia3-2.cdninstagram.com&_nc_ohc=XPrxmdmhgkkAX_bMPm1&oh=ab9dd88c594b750afb099fc236c6bbee&oe=5ED1BAD7" alt="" class="rounded-circle">
+            <img src="/storage/{{ $user->profile->image }}" alt="" class="w-100 rounded-circle">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="/p/create">Add new post</a>
+                @can('update', $user->profile)
+                    <a href="/p/create">Add new post</a>
+                @endcan
             </div>
-            <a href="/profile/{{ $user->id }}/edit">Edit profile</a>
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit profile</a>
+            @endcan
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
