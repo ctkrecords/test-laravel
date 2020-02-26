86c346628a502c5c1e18dcb3dd1f08bb3557b81d
ctkrecords <ctkrecordings@gmail.com>
2020-02-26 17:35:03 -0600

    database credentials modified for Heroku postgres

--- config/database.php
+++ config/database.php
@@ -66,12 +66,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => $DATABASE_URL['url'],
-            'host' => $DATABASE_URL['host'],
-            'port' => $DATABASE_URL['post'],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL['user'],
-            'password' => $DATABASE_URL['pass'],
+            //'url' => env('DATABASE_URL'),
+            'host' => $DATABASE_URL['host'],//env('DB_HOST', '127.0.0.1'),
+            'port' => $DATABASE_URL['port'],//env('DB_PORT', '5432'),
+            'database' => ltrim($DATABASE_URL["path"], "/"),//env('DB_DATABASE', 'forge'),
+            'username' => $DATABASE_URL['user'],//env('DB_USERNAME', 'forge'),
+            'password' => $DATABASE_URL['pass'],//env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
